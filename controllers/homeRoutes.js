ca5e8dae07251d3fea2e15c8d2ca577b6634d5bf
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-09-19 16:18:11 -0500

    added JS for login, logout, and profile event handlers. Updated homeRoutes to fix issues with blogs not returning into profile, set up logout and profile handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Blog, User } = require("../models");
-// const withAuth = require("../utils/auth");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -23,16 +23,49 @@ router.get("/", async (req, res) => {
   }
 });
 
-//get /blog/:id
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [{ model: User, attributes: ["name"] }],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render("blog", {
+      ...blog,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //get /profile withauth
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Blog, attributes: ["name", "description"] }],
+    });
 
-router.get('/login', (req, res) => {
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      loggedIn: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
