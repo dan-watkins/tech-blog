483a9dbed4894f1a47add806d43b2a2671d61157
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-09-20 17:15:00 -0500

    added blog handlebars and updated profile to include delete buttons and links for specific blogs

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,6 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
-  
+
   const name = document.getElementById("blog-post").value.trim();
   const description = document.getElementById("blog-desc").value.trim();
 
@@ -25,7 +25,7 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute("data-id")) {
     const id = event.target.getAttribute("data-id");
 
-    const response = await fetch(`/api/blogs/${id}`, {
+    const response = await fetch(`/api/blog/${id}`, {
       method: "DELETE",
     });
 
